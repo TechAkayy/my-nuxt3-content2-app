ed9546a092bfccc0f9ff4968e46d64b76be6e99a
akkayy <techakayy@gmail.com>
2022-09-07 15:00:21 +1000

    Replaced useDocuments iles with queryContent composable

--- composables/posts.ts
+++ composables/posts.ts
@@ -1,18 +1,29 @@
-import type { Document, PageComponent } from 'iles'
-import { computed } from 'vue'
+// import type { Document, PageComponent } from 'iles'
+// import { computed } from 'vue'
 
-export interface Post extends PageComponent {
-  date: Date
-  author: string
-  title: string
-  twitter: string
+// export interface Post extends PageComponent {
+// 	date: Date
+// 	author: string
+// 	title: string
+// 	twitter: string
+// }
+
+// function byDate(a: Document<Post>, b: Document<Post>) {
+// 	return Number(new Date(b.date)) - Number(new Date(a.date))
+// }
+
+// export function getPosts() {
+// 	const posts = useDocuments<Post>('~/pages/posts')
+// 	return computed(() => posts.value.sort(byDate))
+// }
+
+function byDate(a, b) {
+	return Number(new Date(b.date)) - Number(new Date(a.date))
 }
 
-function byDate (a: Document<Post>, b: Document<Post>) {
-  return Number(new Date(b.date)) - Number(new Date(a.date))
-}
-
-export function getPosts () {
-  const posts = useDocuments<Post>('~/pages/posts')
-  return computed(() => posts.value.sort(byDate))
+export async function getPosts() {
+	const { data } = await useAsyncData('posts', () => queryContent('/').find())
+	return data
+	// const posts = useDocuments('~/pages/posts')
+	// return computed(() => posts.value.sort(byDate))
 }
